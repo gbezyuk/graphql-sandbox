26b9f8498d52ce824b75ad8768347c93cdd12742
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-03-10 11:22:11 +0300

    starwars scheme: episode enum value descriptions

--- backend/starwars.py
+++ backend/starwars.py
@@ -5,5 +5,11 @@ class Episode(graphene.Enum):
     EMPIRE = 5
     JEDI = 6
 
+    @property
+    def description(self):
+        if self == Episode.NEWHOPE:
+            return 'New Hope Episode'
+        return 'Other episode'
+
 # alternative syntax is as follows:
 # Episode = graphene.Enum('Episode', [('NEWHOPE', 4), ('EMPIRE', 5), ('JEDI', 6)])
