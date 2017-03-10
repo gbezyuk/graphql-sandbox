8d6e3f4381ca212ffe92f52efeec126b61ffbb52
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-03-10 11:39:53 +0300

    reformatted

--- backend/mounting-scalars.py
+++ backend/mounting-scalars.py
@@ -1,18 +1,18 @@
 class Person(graphene.ObjectType):
     name = graphene.String()
 
-# Is equivalent to:
-# class Person(graphene.ObjectType):
-#     name = graphene.Field(graphene.String)
-#
+#   Is equivalent to:
+#   name = graphene.Field(graphene.String)
+
 # graphene.Field(graphene.String, to=graphene.String())
 # Is equivalent to:
 # graphene.Field(graphene.String, to=graphene.Argument(graphene.String))
-# (?) @gbezyuk
+# (Argument?) @gbezyuk
+
 
 class Character(graphene.ObjectType):
+    
     name = graphene.String(required=True)
 
-# Is equivalent to:
-# class Character(graphene.ObjectType):
-#    name = graphene.NonNull(graphene.String)
+#   Is equivalent to:
+#   name = graphene.NonNull(graphene.String)
