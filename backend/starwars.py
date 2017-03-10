2fdfaebaeae75635ff8c5d05fb699ce8c3f89797
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-03-10 11:26:21 +0300

    starwars scheme: episode enum comments

--- backend/starwars.py
+++ backend/starwars.py
@@ -13,3 +13,11 @@ class Episode(graphene.Enum):
 
 # alternative syntax is as follows:
 # Episode = graphene.Enum('Episode', [('NEWHOPE', 4), ('EMPIRE', 5), ('JEDI', 6)])
+
+# one can use python enums too:
+# graphene.Enum.from_enum(AlreadyExistingPyEnum)
+# see https://docs.python.org/3/library/enum.html
+# actually, as official docs say, quote:
+# "graphene.Enum uses enum.Enum internally (or a backport if that’s not available)
+# and can be used in the exact same way.
+
