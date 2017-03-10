a336e5406ff6ad46983be68015cc36b71e1d9808
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-03-10 11:36:01 +0300

    question on graphene.Argument fixed in a comment

--- backend/mounting-scalars.py
+++ backend/mounting-scalars.py
@@ -1,9 +1,13 @@
-# class Person(graphene.ObjectType):
-#     name = graphene.String()
+class Person(graphene.ObjectType):
+    name = graphene.String()
+
 # Is equivalent to:
 # class Person(graphene.ObjectType):
 #     name = graphene.Field(graphene.String)
-
+#
 # graphene.Field(graphene.String, to=graphene.String())
 # Is equivalent to:
 # graphene.Field(graphene.String, to=graphene.Argument(graphene.String))
+# (?) @gbezyuk
+
+
